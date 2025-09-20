693fbe5c7412bf483b23cd805c1b598ebbdb879a
sallu-50 <salmanabdullahal0@gmail.com>
2025-09-21 04:24:42 +0600

    u

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,12 +2,12 @@
 
 namespace App\Providers\Filament;
 
+use Illuminate\Support\ServiceProvider;
+use Filament\Panel\Panel;
 use Filament\Pages\Dashboard;
 use Filament\Support\Colors\Color;
 use Filament\Widgets\AccountWidget;
 use Filament\Widgets\FilamentInfoWidget;
-use Filament\Panel;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Session\Middleware\StartSession;
