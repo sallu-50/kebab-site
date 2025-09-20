3bee5a2188cebe1e6cb8251376bf9f1eb45a75d0
sallu-50 <salmanabdullahal0@gmail.com>
2025-09-20 18:28:34 +0600

    fgfdg

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,8 +4,8 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    <a href="{{ route('home') }}">
+                        <x-application-logo class=" w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
